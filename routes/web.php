7cd5588908d23e0917690537c8da13e7562238b8
Arturo Garrido Rosich <agarridorosich@cifpfbmoll.eu>
2023-02-13 20:22:55 +0000

    añadir rutas

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+Route::resource('/posts', PostController::class);
 Route::get('/', function () {
     return view('welcome');
 });
